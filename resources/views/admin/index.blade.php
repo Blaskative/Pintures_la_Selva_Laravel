8e7f00342748d57db28c46936fd9fcbe3996bf9c
Matthew <grmatthewlz@gmail.com>
2021-10-21 03:44:13 +0200

    Adminlte

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,27 @@
 @extends('adminlte::page')
+
+@section('title','Pintures la Selva')
+    
+@section('content_header')
+
+<h1>Pintures la Selva</h1>
+
+@stop
+
+@section('content')
+
+<p>Welcome to a beautiful admin panel.</p>
+
+@stop
+
+@section('css')
+
+<link rel="stylesheet" href="/css/admin_custom.css">
+
+@stop
+
+@section('js')
+
+<script>console.log('hi');</script>
+
+@stop
